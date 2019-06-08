f20fadd31a9737466c6b7f0ec7652b118e8dfac4
Sheviak <sheviak.k@gmail.com>
2019-06-08 10:55:45 +0300

    Bug fix + UI
    - реализовал создание приложения одного экземпляра
    - UI добавил плавную прокрутку

--- MyCash/App.xaml.cs
+++ MyCash/App.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 
 namespace MyCash
@@ -13,5 +13,31 @@ namespace MyCash
     /// </summary>
     public partial class App : Application
     {
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr handle, int cmdShow);
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr handle);
+
+        private Mutex mutex = new Mutex(false, "WpfApplication");
+
+        public App()
+        {
+            if (!mutex.WaitOne(500, false))
+            {
+                MessageBox.Show("Копия какого-то приложения работает!", "Ошибка");
+                string processName = Process.GetCurrentProcess().ProcessName;
+                Process process = Process.GetProcesses().Where(p => p.ProcessName == processName).FirstOrDefault();
+                if (process != null)
+                {
+                    IntPtr handle = process.MainWindowHandle;
+                    ShowWindow(handle, 1);
+                    SetForegroundWindow(handle);
+                }
+                this.Shutdown();
+                return;
+            }
+            InitializeComponent();
+        }
     }
 }
